35117c02e69b7e38797cc040f3b05e445357edcc
Oliver King <kingoliver@microsoft.com>
2022-01-19 14:29:36 -0600

    change port

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-PORT = 8000
+PORT = 1323
 
-@app.route("/")
+# catches all urls
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
 def hello():
     return "Hello World!"
 
