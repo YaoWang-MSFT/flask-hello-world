50bb093b021a8db651c6125e1c2919e45fc78b47
Oliver King <kingoliver@microsoft.com>
2022-01-19 13:58:36 -0600

    update port

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+PORT = 8000
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(port=PORT)
